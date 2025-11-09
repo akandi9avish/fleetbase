b7ed1fbf0c0d633450e502e6cc5e77afd061fd1e
akandi6avish <33635695+akandi9avish@users.noreply.github.com>
2025-11-09 17:41:23 -0500

    Fix: Add ALLOWED_ORIGINS to CORS configuration for www.reeup.co iframe support

--- api/config/cors.php
+++ api/config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(['http://localhost:4200', 'https://healthcheck.railway.app', env('CONSOLE_HOST'), Utils::addWwwToUrl(env('CONSOLE_HOST')), ...Utils::arrayFrom(env('FRONTEND_HOSTS', ''))]),
+    'allowed_origins' => array_filter(['http://localhost:4200', 'https://healthcheck.railway.app', env('CONSOLE_HOST'), Utils::addWwwToUrl(env('CONSOLE_HOST')), ...Utils::arrayFrom(env('ALLOWED_ORIGINS', '')), ...Utils::arrayFrom(env('FRONTEND_HOSTS', ''))]),
 
     'allowed_origins_patterns' => [],
 
